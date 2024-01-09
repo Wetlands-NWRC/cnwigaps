bf8bd7f2314eb928eacdccc8819d987a881ea580
Ryan Hamilton <ryan.hamilton@ec.gc.ca>
2024-01-09 13:47:45 -0500

    Refactor DataManifest and DataProcessor classes

--- cnwigaps/dataproc.py
+++ cnwigaps/dataproc.py
@@ -1,7 +1,11 @@
 import os
+
+from pathlib import Path
+
 import geopandas as gpd
 import pandas as pd
 
+from cnwigaps.data.data import load_data
 
 """
 get data -> needs to get all shapefile paths under the data directory
@@ -21,31 +25,63 @@ combine the data -> needs to combine the data into a single geopandas dataframe
 
 class DataManifest:
     def __init__(self, root: str):
-        self.root = root
-        self.manifest = None
+        self.manifest = Path(root)
+        self.groupby_col = "ECOZONE_ID"
 
-    def get_files(self):
-        pass
+    def __iter__(self):
+        for idx, group in self.manifest.groupby(self.groupby_col):
+            yield idx, group
+
+    @property
+    def manifest(self):
+        return self._manifest
+
+    @manifest.setter
+    def manifest(self, root: str):
+        self._manifest = self._create_manifest(root)
+
+    @staticmethod
+    def _create_manifest(root: Path) -> pd.DataFrame:
+        """creates the data manifest"""
+        files = list(root.glob("**/*.shp"))
+        M = {"trainingPoints": 1, "validationPoints": 2, "region": 3}
+        manifest = pd.DataFrame(files, columns=["file_path"])
+        manifest["ECOREGION_ID"] = (
+            manifest["file_path"].astype(str).str.extract(r"(\b\d{1,3}\b)").astype(int)
+        )
+        manifest["type"] = (
+            manifest["file_path"]
+            .astype(str)
+            .str.extract(r"(\btrainingPoints\b|\bvalidationPoints\b|\bregion\b)")
+        )
+        manifest["type"] = manifest["type"].map(M)
+
+        manifest = manifest[manifest["file_path"].astype(str).str.contains(r"(\.shp)")]
+
+        manifest = manifest.sort_values(by=["ECOREGION_ID", "type"])
+        manifest = manifest.reset_index(drop=True)
+
+        eco = load_data()
+
+        manifest = pd.merge(manifest, eco, on="ECOREGION_ID", how="inner")
+
+        return manifest[["file_path", "ECOREGION_ID", "type", "ECOZONE_ID"]]
 
 
 class DataProcessor:
-    def __init__(self, data_dir: str):
-        self.manifest = None
-        self.train = []
-        self.test = []
-        self.region = []
-        self.gdf = None
-        self.lookup = None
+    def __init__(self, manifest: DataManifest):
+        self.manifest = manifest
+        self.training = {}
+        self.regions = {}
 
-    def get_data_paths(self) -> list[str]:
-        """gets all the files in the data directory"""
-        pass
-
-    def load_data_paths(self):
+    def load_files(self):
         # load the training file
-        for file in self.get_files():
-            pass
-        pass
+        for idx, group in self.manifest:
+            # training_files
+            tmp = []
+            for _, row in group.iterrows():
+                if row["type"] == 1 or row["type"] == 2:
+                    pass
 
     def convert_crs(self):
         pass
