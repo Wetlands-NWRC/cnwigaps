deb850454590b9078a3de9662d0637a78203f259
Ryan Hamilton <ryan.hamilton@ec.gc.ca>
2024-01-23 14:46:28 -0500

    Add data classes and processors for training and region data processing

--- cnwigaps/dataproc.py
+++ cnwigaps/dataproc.py
@@ -1,5 +1,6 @@
 import os
 
+from dataclasses import dataclass
 from pathlib import Path
 
 import geopandas as gpd
@@ -7,6 +8,75 @@ import pandas as pd
 from zipfile import ZipFile
 
 from cnwigaps.data.data import load_data
+
+
+@dataclass
+class TrainingData:
+    """
+    Represents training data.
+    """
+
+    data: gpd.GeoDataFrame
+
+
+@dataclass
+class RegionData:
+    """
+    Represents region data.
+    """
+
+    data: gpd.GeoDataFrame
+
+
+class TrainingDataProcessor:
+    def __init__(self) -> None:
+        self.gdfs = []
+
+    def add(self, gdf: gpd.GeoDataFrame) -> None:
+        self.gdfs.append(gdf)
+        return None
+
+    def process(self, n_rows=10_000, balance: bool = False) -> TrainingData:
+        gdf = gpd.GeoDataFrame(pd.concat(self.gdfs))
+        gdf.to_crs(epsg=4326, inplace=True)
+
+        labels = gdf["class_name"].unique().tolist()
+        int_labels = list(range(1, len(labels) + 1))
+
+        label_map = dict(zip(labels, int_labels))
+        gdf["class_name"] = gdf["class_name"].map(label_map)
+
+        if balance:
+            # target number of observations per class
+            n = n_rows // len(gdf["class_name"].unique().tolist())
+
+            # if any class has n observations less then the target use that value for all labels
+            grouped_counts = gdf.groupby("class_name").size().reset_index(name="Count")
+            min_count = grouped_counts["Count"].min()
+
+            if n > min_count:
+                n = min_count
+
+            selected = gdf.groupby("class_name").sample(n=n)
+        else:
+            selected = gdf.sample(n=n_rows)
+        selected = selected.reset_index(drop=True)
+        return TrainingData(selected)
+
+
+class RegionDataProcessor:
+    def __init__(self) -> None:
+        self.gdfs = []
+
+    def add(self, gdf: gpd.GeoDataFrame) -> None:
+        self.gdfs.append(gdf)
+        return None
+
+    def process(self) -> RegionData:
+        gdf = gpd.GeoDataFrame(pd.concat(self.gdfs))
+        gdf.to_crs(epsg=4326, inplace=True)
+        gdf = gdf.dissolve(by="ECOZONE_ID")
+        return RegionData(gdf)
 
 
 class DataManifest:
@@ -90,107 +160,27 @@ class DataManifest:
         return manifest[["file_path", "ECOREGION_ID", "type", "ECOZONE_ID"]]
 
 
-def process_data_manifest(manifest: DataManifest):
-    n_rows = 10_000
-    # load data
-    output = {}
+def process_data_manifest(
+    manifest: DataManifest, n_rows: int = 10_000, balance: bool = False
+) -> tuple[TrainingData, RegionData]:
     for _, group in manifest:
         # sub routine that loads the data before moving to processing
         ## Load data
-        tmp, tmp_r = [], []
+        tdp = TrainingDataProcessor()
+        rdp = RegionDataProcessor()
         for idx, row in group.iterrows():
             if row["type"] == 1 or row["type"] == 2:
                 gdf = gpd.read_file(row["file_path"])
                 gdf["type"] = row["type"]
                 gdf["ECOZONE_ID"] = row["ECOZONE_ID"]
-                tmp.append(gdf)
+                tdp.add(gdf)
             if row["type"] == 3:
                 gdf = gpd.read_file(row["file_path"])
                 gdf["type"] = row["type"]
                 gdf["ECOZONE_ID"] = row["ECOZONE_ID"]
-                tmp_r.append(gdf)
-        ## Process data
+                rdp.add(gdf)
 
-        ### Processing for training and validation data
-        gdf = gpd.GeoDataFrame(pd.concat(tmp))
-        gdf.to_crs(epsg=4326, inplace=True)
+    td = tdp.process(n_rows=n_rows, balance=balance)
+    rd = rdp.process()
 
-        labels = gdf["class_name"].unique().tolist()
-        int_labels = list(range(1, len(labels) + 1))
-
-        label_map = dict(zip(labels, int_labels))
-        gdf["class_name"] = gdf["class_name"].map(label_map)
-
-        # target number of observations per class
-        n = n_rows // len(gdf["class_name"].unique().tolist())
-
-        # if any class has n observations less then the target use that value for all labels
-        grouped_counts = gdf.groupby("class_name").size().reset_index(name="Count")
-        min_count = grouped_counts["Count"].min()
-
-        if n > min_count:
-            n = min_count
-
-        selected = gdf.groupby("class_name").sample(n=n)
-        selected = selected.reset_index(drop=True)
-
-        ### Processing for region data
-        gdf_r = gpd.GeoDataFrame(pd.concat(tmp_r))
-        gdf_r.to_crs(epsg=4326, inplace=True)
-        gdf_r = gdf_r.dissolve(by="ECOZONE_ID")
-
-        output[idx] = {"t": selected, "r": gdf_r}
-    return output
-
-
-def split_and_zip(
-    gdf: gpd.GeoDataFrame, groupby_col: str, where, file_prefix: str = None
-) -> None:
-    """
-    Compresses and archives GeoDataFrame groups based on a specified column.
-
-    Args:
-        gdf (gpd.GeoDataFrame): The GeoDataFrame containing the data to be grouped and compressed.
-        groupby_col (str): The column name to group the GeoDataFrame by.
-        where: The directory path where the compressed files will be saved.
-        file_prefix (str, optional): The prefix to be used for the compressed file names. Defaults to None.
-
-    Returns:
-        None
-    """
-
-    scratch = where / "scratch"
-    scratch.mkdir(exist_ok=True)
-
-    for _, group in gdf.groupby(groupby_col):
-        # save each group
-        file_prefix = file_prefix or "features"
-        group.to_file(scratch / f"{file_prefix}_{_}.shp", driver="ESRI Shapefile")
-        archive_name = f"{file_prefix}_{_}"
-        # compress each dataset
-        try:
-            with ZipFile(scratch / f"{archive_name}.zip", "w") as zip:
-                zip.write(scratch / f"{archive_name}.shp")
-                zip.write(scratch / f"{archive_name}.dbf")
-                zip.write(scratch / f"{archive_name}.prj")
-                zip.write(scratch / f"{archive_name}.shx")
-                zip.write(scratch / f"{archive_name}.cpg")
-        except FileExistsError:
-            continue
-
-        # move the zip file to the processed directory
-        (where / "zipped").mkdir(exist_ok=True)
-        (scratch / f"{archive_name}.zip").rename(
-            where / "zipped" / f"{archive_name}.zip"
-        )
-
-        # clean up the scratch directory
-        (scratch / f"{archive_name}.shp").unlink()
-        (scratch / f"{archive_name}.dbf").unlink()
-        (scratch / f"{archive_name}.prj").unlink()
-        (scratch / f"{archive_name}.shx").unlink()
-        (scratch / f"{archive_name}.cpg").unlink()
-        # output processed/shapefile AND processed/zipped
-
-    # remove the scratch directory
-    scratch.rmdir()
+    return td, rd
